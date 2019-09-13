b273932e94617e92dd85e1cbc1b142a787ae7e5f
Hefelux <hernan.feliu.a@gmail.com>
2019-09-13 20:11:07 -0300

    Updates

---
+++ precache-manifest.58814da86b936debbe6213cc5caed951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcab38971170dbc45d7a905d09781a41",
+    "url": "/veterinary_appointment/index.html"
+  },
+  {
+    "revision": "87b412faf35154efce8d",
+    "url": "/veterinary_appointment/static/css/main.a5ad472b.chunk.css"
+  },
+  {
+    "revision": "03cdc56efdc12cc0f797",
+    "url": "/veterinary_appointment/static/js/2.6efcacb1.chunk.js"
+  },
+  {
+    "revision": "87b412faf35154efce8d",
+    "url": "/veterinary_appointment/static/js/main.529d6508.chunk.js"
+  },
+  {
+    "revision": "6d78e26b6814105539c8",
+    "url": "/veterinary_appointment/static/js/runtime~main.bdae56cb.js"
+  }
+]);
